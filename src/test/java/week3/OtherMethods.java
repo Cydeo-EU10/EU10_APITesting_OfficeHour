ec0d263698cafaacff546afedf898b30b7bc4d5d
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-05-24 20:20:39 +0300

    week 3 is created

---
+++ src/test/java/week3/OtherMethods.java
@@ -0,0 +1,62 @@
+package week3;
+
+import io.restassured.*;
+import io.restassured.http.*;
+import io.restassured.response.*;
+import org.junit.jupiter.api.*;
+import week2.*;
+import week2.POJO.*;
+
+import java.util.*;
+
+public class OtherMethods extends SpartanTestBase {
+
+
+    /*
+    postman collection:
+    https://api.postman.com/collections/14823187-c18d5c5e-0b8a-4d16-9725-98cc9aaf7588?access_key=PMAT-01H0NGA414Y4M9QDE5BJMXT9DP
+
+     */
+
+    // post method
+    @Test
+    public void test1(){
+        /*
+        3 ways to provide body to api
+        1. as String
+        2. as Map
+        3. as POJO
+         */
+
+        // string
+        String bodyString = "{\n" +
+                "  \"gender\": \"Male\",\n" +
+                "  \"name\": \"Hanks\",\n" +
+                "  \"phone\": 1236547895\n" +
+                "}";
+
+        // map
+        Map<String, Object> bodyMap = new HashMap<>();
+        bodyMap.put("name","Jeniffer");
+        bodyMap.put("gender","Female");
+        bodyMap.put("phone",1236547895L);
+
+        // pojo or java object
+        SingleSpartan bodyPojo = new SingleSpartan();
+        bodyPojo.setName("Jack");
+        bodyPojo.setGender("Male");
+        bodyPojo.setPhone(5468792135L);
+
+        Response response = RestAssured.given().accept(ContentType.JSON) // telling api that we only receive json format as response
+                .and().contentType(ContentType.JSON)// telling api that the data we sent is json format
+                .and().body(bodyPojo)
+                .when().post("/api/spartans");
+
+        Assertions.assertEquals(201,response.statusCode());
+
+    }
+
+
+
+
+}
